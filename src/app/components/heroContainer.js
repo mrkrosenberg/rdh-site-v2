aee6670212fca16f7dd9c11648e0c1d5234d6b82
Mark <mrk.rosenberg@gmail.com>
2024-02-12 12:10:28 -0700

    starts to build hero contianer

---
+++ src/app/components/heroContainer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// components
+import navBar from './navBar';
+
+function heroContainer() {
+  return (
+    <div>
+        <navBar />
+        <div className="hero-container">
+
+        </div>
+    </div>
+  )
+};
+
+export default heroContainer;
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
 
+// components
+import heroContainer from "./components/heroContainer";
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        hello
+        <heroContainer />
       </div>
     </main>
   );
